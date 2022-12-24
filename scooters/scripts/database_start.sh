27b3a529fa7888706e14d24c0f1952fa9283003e
vtitoff <v.titoff2010@gmail.com>
2022-12-24 16:39:23 +0300

    commit

--- scooters/scripts/database_start.sh
+++ scooters/scripts/database_start.sh
@@ -1,1 +1,1 @@
-docker run -e POSTGRES_PASSWORD=hackme -e POSTGRES_USER=scootersuser -e POSTGRES_DB=scooters -p 5432:5432 -d postgres
+docker run -e POSTGRES_PASSWORD=hackme -e POSTGRES_USER=scootersuser -e POSTGRES_DB=scooters -p 5433:5432 -d postgres
